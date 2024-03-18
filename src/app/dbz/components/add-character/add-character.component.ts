60195d387bd3cb59a452e829dcfa0460fc78b5c2
Rafael <aafrelx@gmail.com>
2024-03-18 16:56:23 -0700

    feat: fin sección 5, dbz characters

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './add-character.component.css'
 })
 export class AddCharacterComponent {
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
+  public character: Character = {
+    id: uuid(),
+    name: 'Vulma',
+    power: 0
+  };
+
+  emitCharacter(): void {
+    if ( this.character.name.length === 0) return;
+    this.onNewCharacter.emit(this.character);
+
+    this.character = { id: uuid(), name: '', power: 0 };
+  }
 }
